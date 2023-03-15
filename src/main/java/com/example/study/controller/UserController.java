018b418a91c9c731ea7865d9793b42d69d8f286c
2002yyf <1207835084@qq.com>
2023-03-15 19:35:49 +0800

    1

--- src/main/java/com/example/study/controller/UserController.java
+++ src/main/java/com/example/study/controller/UserController.java
@@ -19,28 +19,28 @@ public class UserController {
     @PostMapping("/save")//用@RequestMapping，则什么请求类型都可以
     //restful接口风格，可以用不同的请求方式实现不同的效果
     //使用@PathVariable注解，让方法参数的值对应绑到一个URL模板变量上
-    public String saveUser(@RequestBody UserRegisterDto userRegisterDto){
+    public Result saveUser(@RequestBody UserRegisterDto userRegisterDto){
         if(userRegisterDto ==null){
-            return "操作失败";
+            return Result.error("1","对象为空");
         }
-        if(userRegisterDto.getUsername()==null|| userRegisterDto.getPassword()==null|| userRegisterDto.getPhone()==null){
-            return "操作失败";
+        if(userRegisterDto.getUsername()==null|| userRegisterDto.getPassword()==null){
+            return Result.error("2","信息不足");
         }
         User user=new User();
         user.setUsername(userRegisterDto.getUsername());
         user.setPassword(userRegisterDto.getPassword());
         user.setPhone(userRegisterDto.getPhone());
         if(!(userService.selectUser(user)==null)){
-            return "此账号与密码已重复";
+            return Result.error("3","账号已存在");
         }
         userService.saveUser(user);
-        return "操作成功";
+        return Result.success(userService.selectUser(user),"注册成功");
     }
 
     @PostMapping("/login")
     public Result loginIn(@RequestBody UserLoginDto userLoginDto){
         if(userLoginDto==null){
-            return Result.error("0","账号密码没输入");
+            return Result.error("2","账号密码没输入");
         }
         User user=new User();
         user.setUsername(userLoginDto.getUsername());
