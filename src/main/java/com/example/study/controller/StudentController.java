aafcac1d2aa02c87a48b11fea42dac9288b1e21c
2002yyf <1207835084@qq.com>
2023-04-12 18:38:18 +0800

    1

--- src/main/java/com/example/study/controller/StudentController.java
+++ src/main/java/com/example/study/controller/StudentController.java
@@ -12,10 +12,7 @@ import com.example.study.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @CrossOrigin
 @RestController
--- src/main/resources/study.sql
+++ src/main/resources/study.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 28/03/2023 14:59:27
+ Date: 12/04/2023 18:37:47
 */
 
 SET NAMES utf8mb4;
@@ -22,13 +22,19 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `course`;
 CREATE TABLE `course`  (
-  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `credit` int(0) NULL DEFAULT NULL,
+  `hour` int(0) NULL DEFAULT NULL,
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '课程类型：专业课、选修课等',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of course
 -- ----------------------------
+INSERT INTO `course` VALUES (1, '数据结构', 4, 64, '必修课');
+INSERT INTO `course` VALUES (2, '操作系统', 5, 32, '必修课');
 
 -- ----------------------------
 -- Table structure for grade
@@ -47,6 +53,27 @@ CREATE TABLE `grade`  (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for score
+-- ----------------------------
+DROP TABLE IF EXISTS `score`;
+CREATE TABLE `score`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `sid` int(0) NULL DEFAULT NULL,
+  `cid` int(0) NULL DEFAULT NULL,
+  `score` int(0) NULL DEFAULT NULL,
+  `time` date NULL DEFAULT NULL,
+  `gpa` double(255, 2) NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of score
+-- ----------------------------
+INSERT INTO `score` VALUES (1, 1, 2, 90, '2022-10-15', 4.50, '通过');
+INSERT INTO `score` VALUES (2, 1, 1, 80, '2022-01-06', 5.00, '通过');
+
+-- ----------------------------
 -- Table structure for student
 -- ----------------------------
 DROP TABLE IF EXISTS `student`;
@@ -54,21 +81,21 @@ CREATE TABLE `student`  (
   `id` int(0) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `gender` bit(1) NULL DEFAULT NULL COMMENT '0女，1男',
-  `birthday` datetime(0) NULL DEFAULT NULL,
+  `birthday` date NULL DEFAULT NULL,
   `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `address` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `admission_date` datetime(0) NULL DEFAULT NULL COMMENT '入学日期',
-  `graduation_date` datetime(0) NULL DEFAULT NULL COMMENT '毕业日期',
+  `admission_date` date NULL DEFAULT NULL COMMENT '入学日期',
+  `graduation_date` date NULL DEFAULT NULL COMMENT '毕业日期',
   `major` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `gpa` decimal(3, 2) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of student
 -- ----------------------------
-INSERT INTO `student` VALUES (1, 'abc', b'1', '2002-08-18 00:00:00', '123', '123', '杭州', '2002-08-18 00:00:00', '2002-08-18 00:00:01', '计算机1', 4.24);
+INSERT INTO `student` VALUES (1, 'abcd', b'1', '2022-01-03', NULL, '2222222222151', NULL, NULL, NULL, NULL, 3.60);
 
 -- ----------------------------
 -- Table structure for user
