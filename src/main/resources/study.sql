72b52d5d903bac3f78b99b53183ba0ab2dc1642a
2002yyf <1207835084@qq.com>
2023-04-12 19:36:54 +0800

    增加教师表、机构表

--- src/main/resources/study.sql
+++ src/main/resources/study.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 12/04/2023 18:56:58
+ Date: 12/04/2023 19:36:26
 */
 
 SET NAMES utf8mb4;
@@ -53,6 +53,24 @@ CREATE TABLE `grade`  (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for institution
+-- ----------------------------
+DROP TABLE IF EXISTS `institution`;
+CREATE TABLE `institution`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of institution
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for score
 -- ----------------------------
 DROP TABLE IF EXISTS `score`;
@@ -80,6 +98,7 @@ DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student`  (
   `id` int(0) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `snum` int(0) NULL DEFAULT NULL COMMENT '学号',
   `gender` bit(1) NULL DEFAULT NULL COMMENT '0女，1男',
   `birthday` date NULL DEFAULT NULL,
   `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
@@ -89,13 +108,38 @@ CREATE TABLE `student`  (
   `graduation_date` date NULL DEFAULT NULL COMMENT '毕业日期',
   `major` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `gpa` decimal(3, 2) NULL DEFAULT NULL,
+  `grade` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `introduction` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '个人简介',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of student
 -- ----------------------------
-INSERT INTO `student` VALUES (1, 'abcd', b'1', '2022-01-03', NULL, '2222222222151', NULL, NULL, NULL, NULL, 3.60);
+INSERT INTO `student` VALUES (1, 'abcd', NULL, b'1', '2022-01-03', NULL, '2222222222151', NULL, NULL, NULL, NULL, 3.60, NULL, NULL);
+
+-- ----------------------------
+-- Table structure for teacher
+-- ----------------------------
+DROP TABLE IF EXISTS `teacher`;
+CREATE TABLE `teacher`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `tnum` int(0) NOT NULL COMMENT '职工号',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `position` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '职位',
+  `birthday` date NULL DEFAULT NULL,
+  `appointment_time` date NULL DEFAULT NULL,
+  `leave_time` date NULL DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `production` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of teacher
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for user
@@ -105,7 +149,6 @@ CREATE TABLE `user`  (
   `id` int(0) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `isDeleted` int(0) NULL DEFAULT NULL,
   `identity` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
@@ -114,9 +157,9 @@ CREATE TABLE `user`  (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'abc', '123', NULL, 0, '1');
-INSERT INTO `user` VALUES (2, 'abcd', '1', NULL, 0, NULL);
-INSERT INTO `user` VALUES (3, 'abcd', '3', '123', 0, NULL);
-INSERT INTO `user` VALUES (4, 'abcd', '4', NULL, 0, NULL);
+INSERT INTO `user` VALUES (1, 'abc', '123', 0, '1');
+INSERT INTO `user` VALUES (2, 'abcd', '1', 0, NULL);
+INSERT INTO `user` VALUES (3, 'abcd', '3', 0, NULL);
+INSERT INTO `user` VALUES (4, 'abcd', '4', 0, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
