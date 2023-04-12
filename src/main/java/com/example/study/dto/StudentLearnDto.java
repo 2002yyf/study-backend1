b8f5966fa37318dcc3f251f7c7b7e0b48500223d
2002yyf <1207835084@qq.com>
2023-04-12 19:08:03 +0800

    修改日期格式

--- src/main/java/com/example/study/dto/StudentLearnDto.java
+++ src/main/java/com/example/study/dto/StudentLearnDto.java
@@ -1,5 +1,6 @@
 package com.example.study.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.util.Date;
 @NoArgsConstructor
 public class StudentLearnDto {
     private Integer cid;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date time;
     private String name;
     private Integer credit;
